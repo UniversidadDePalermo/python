c488d3caf7885284ba9ba4b6955d67c0ee47ad64
Esteban Borai <estebanborai@gmail.com>
2022-09-02 18:46:03 -0400

    fix: add validations

--- 2022_09_02/sum.py
+++ 2022_09_02/sum.py
@@ -1,11 +1,14 @@
-numero_1 = int(input('Ingrese un numero: '))
-numero_2 = int(input('Ingrese un segundo numero: '))
+inicio = int(input('Ingrese un numero: '))
+fin = int(input('Ingrese un segundo numero: '))
 
-contador = numero_1 + 1
-acc = 0
+if inicio < fin:
+  contador = inicio + 1
+  acc = 0
 
-while contador < numero_2:
-  acc = acc + contador
-  contador = contador + 1
+  while contador < fin:
+    acc = acc + contador
+    contador = contador + 1
 
-print(f'La suma es: {acc}')
+  print(f'La suma es: {acc}')
+else:
+  print('El valor del incio debe ser menor al valor del fin')

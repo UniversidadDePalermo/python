927a0cfb22af395e4eade1a0591009380cc44dd1
Esteban Borai <estebanborai@gmail.com>
2022-09-13 19:36:45 -0300

    fix: improve number operations with validation

--- 05_activities_2/08_operations.py
+++ 05_activities_2/08_operations.py
@@ -1,19 +1,25 @@
-FACTORIAL = 1
-ELEVAR_POTENCIA = 2
+OPC_FACTORIAL = 1
+OPC_ELEVAR_POTENCIA = 2
+OPC_NATURALEZA_NUMERO = 3
 
 mensaje = 'Ingrese un número entero positivo: '
 numero = int(input(mensaje))
 iteracion = 1
 
 while numero >= 0 or iteracion <= 100:
-  iteracion = iteracion + 1
-
-  print(f'{FACTORIAL} = Calcular el factorial para "{numero}"')
-  print(f'{ELEVAR_POTENCIA} = Elevar "{numero}" a una potencia')
-  print(f'3. Determina naturaleza del número (par, impar o nulo)')
+  print(f'{OPC_FACTORIAL} = Calcular el factorial para "{numero}"')
+  print(f'{OPC_ELEVAR_POTENCIA} = Elevar "{numero}" a una potencia')
+  print(f'{OPC_NATURALEZA_NUMERO} = Determina naturaleza del número (par, impar o nulo)')
   operacion = int(input('Opción: '))
 
-  if operacion == FACTORIAL:
+  while operacion < OPC_FACTORIAL or operacion > OPC_NATURALEZA_NUMERO:
+    print(f'La operacion "{operacion}", no es válida\n')
+    print(f'{OPC_FACTORIAL} = Calcular el factorial para "{numero}"')
+    print(f'{OPC_ELEVAR_POTENCIA} = Elevar "{numero}" a una potencia')
+    print(f'{OPC_NATURALEZA_NUMERO} = Determina naturaleza del número (par, impar o nulo)')
+    operacion = int(input('Opción: '))
+
+  if operacion == OPC_FACTORIAL:
     factorial = 1
 
     if numero > 0:
@@ -25,11 +31,11 @@ while numero >= 0 or iteracion <= 100:
       print(f'El factorial para {numero} es {factorial}')
     else:
       print(f'Los numeros negativos como {numero} no tienen valores factoriales')
-  elif operacion == ELEVAR_POTENCIA:
+  elif operacion == OPC_ELEVAR_POTENCIA:
     exponente = int(input('Ingrese exponente para elevar: '))
 
     print(f'{numero}^{exponente} = {numero ** exponente}')
-  else:
+  elif operacion == OPC_NATURALEZA_NUMERO:
     if numero == 0:
       print(f'El numero {numero} es nulo')
     elif numero % 2 == 0:
@@ -37,4 +43,5 @@ while numero >= 0 or iteracion <= 100:
     else:
       print(f'El numero {numero} es impar')
 
+  iteracion = iteracion + 1
   numero = int(input(mensaje))

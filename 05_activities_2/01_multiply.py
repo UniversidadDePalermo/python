2ea23bb8224d24af41c7e81cae072e689a722589
Esteban Borai <estebanborai@gmail.com>
2022-09-13 14:36:38 -0300

    fix: remove forcing while inputs

--- 05_activities_2/01_multiply.py
+++ 05_activities_2/01_multiply.py
@@ -1,10 +1,9 @@
-debe_preguntar = True
-
-while debe_preguntar:
-  numero = int(input('Ingrese un numero: '))
+mensaje = 'Ingrese un numero: '
+numero = int(input(mensaje))
+while numero % 3 != 0:
+  numero = int(input(mensaje))
 
   if numero % 3 == 0:
-    debe_preguntar = False
     print(f'{numero} es multiplo de 3')
   else:
     print(f'{numero} no es multiplo de 3')
--- 05_activities_2/03_enter_numbers.py
+++ 05_activities_2/03_enter_numbers.py
@@ -1,12 +1,11 @@
-debe_preguntar = True
 total_num_positivo = 0
 
-while debe_preguntar:
-  numero = int(input('Ingrese un número: '))
+mensaje = 'Ingrese un número [0 para finalizar]: '
+numero = int(input(mensaje))
+while numero > 0:
+  numero = int(input(mensaje))
 
-  if numero == 0:
-    debe_preguntar = False
-  elif numero > 0:
+  if numero > 0:
     total_num_positivo += 1
 
 print(f'Total números positivos: {total_num_positivo}')

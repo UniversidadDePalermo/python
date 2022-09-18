431c77ae210251cd8305223405b0a479a668c781
Esteban Borai <estebanborai@gmail.com>
2022-09-18 12:54:47 -0300

    feat: counters for article totals

--- 05_activities_2/11_cash_register.py
+++ 05_activities_2/11_cash_register.py
@@ -4,10 +4,19 @@ print(mensaje_codigos_de_articulo)
 codigo_de_articulo = int(input('Ingrese código de articulo: '))
 
 total_general_facturado = 0
+
 cantidad_articulo_1 = 0
 cantidad_articulo_3 = 0
 cantidad_articulo_5 = 0
 cantidad_articulo_7 = 0
+
+facturas_articulo_1 = 0
+facturas_articulo_3 = 0
+facturas_articulo_5 = 0
+facturas_articulo_7 = 0
+
+numero_de_factura_mayor_valor = 0
+valor_de_factura_mayor_valor = 0
 
 while codigo_de_articulo == 1 or codigo_de_articulo == 3 or codigo_de_articulo == 5 or codigo_de_articulo == 7:
   numero_de_factura = int(input('Ingrese número de factura: '))
@@ -28,21 +37,44 @@ while codigo_de_articulo == 1 or codigo_de_articulo == 3 or codigo_de_articulo =
 
   if codigo_de_articulo == 1:
     cantidad_articulo_1 = cantidad_articulo_1 + cantidad_del_articulo
+    facturas_articulo_1 += 1
 
   if codigo_de_articulo == 3:
     cantidad_articulo_3 = cantidad_articulo_3 + cantidad_del_articulo
+    facturas_articulo_3 += 1
 
   if codigo_de_articulo == 5:
     cantidad_articulo_5 = cantidad_articulo_5 + cantidad_del_articulo
+    facturas_articulo_5 += 1
 
   if codigo_de_articulo == 7:
     cantidad_articulo_7 = cantidad_articulo_7 + cantidad_del_articulo
+    facturas_articulo_7 += 1
 
+  # a. Total de cada factura
   print(f'Total: $ {total_factura}')
+
+  # f. Nro. de factura con mayor valor (en $)
+  if total_factura > valor_de_factura_mayor_valor:
+    valor_de_factura_mayor_valor = total_factura
+    numero_de_factura_mayor_valor = numero_de_factura
+
   codigo_de_articulo = int(input('Ingrese código de articulo: '))
 
+# b. Total general facturado
 print(f'Total general: $ {total_general_facturado}')
+
+# c. Cantidad vendida (en unidades) para cada uno de los artículos.
 print(f'Total articulo 1:  {cantidad_articulo_1}')
 print(f'Total articulo 3:  {cantidad_articulo_3}')
 print(f'Total articulo 5:  {cantidad_articulo_5}')
 print(f'Total articulo 7:  {cantidad_articulo_7}')
+
+# d. Total de artículos vendidos
+print(f'Cantidad de artículos vendidos: {cantidad_articulo_1 + cantidad_articulo_3 + cantidad_articulo_5 + cantidad_articulo_7}')
+
+# e. Cantidad de facturas emitidas para cada uno de los artículos.
+print(f'Total facturas articulo 1:  {facturas_articulo_1}')
+print(f'Total facturas articulo 3:  {facturas_articulo_3}')
+print(f'Total facturas articulo 5:  {facturas_articulo_5}')
+print(f'Total facturas articulo 7:  {facturas_articulo_7}')

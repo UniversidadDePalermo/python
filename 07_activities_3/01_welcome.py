b0a5dc585b81d28e4e10026b0232c6819d939af9
canabal97 <112731537+canabal97@users.noreply.github.com>
2022-10-26 19:28:12 -0300

    Update 01_welcome.py

--- 07_activities_3/01_welcome.py
+++ 07_activities_3/01_welcome.py
@@ -1,4 +1,33 @@
 #1#
 
-arreglo = [1,2,3]
-print(len(arreglo))
+arreglo = []
+
+for i in range(3):
+	numero = int(input("Ingrese un numero: "))
+	arreglo.append(numero)
+	
+print(f"El numero de componentes del arreglo es de {len(a)}")
+	
+for i in range(len(arreglo)):
+	print(arreglo[i], end = " ")
+
+
+------------------------------------------------------------------------------------------------------
+
+#CON FUNCIONES#
+
+def ingresar(arreglo):
+    for i in range(3):
+        numero = int(input("Ingrese un numero: "))
+        arreglo.append(numero)
+
+def mostrar(arreglo):
+    for i in range(len(arreglo)):
+        print(arreglo[i], end = " ")
+
+
+a = []
+
+ingresar(a)
+print(f"El numero de componentes del arreglo es de {len(a)}")
+mostrar(a)

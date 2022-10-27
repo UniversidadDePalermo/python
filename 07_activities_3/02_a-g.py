d66b6c06cfc65a7fdc38f3e85b44b4b88e0f2aa2
canabal97 <112731537+canabal97@users.noreply.github.com>
2022-10-27 11:03:17 -0300

    Update 02_a-g.py
    Segundo ejercicio corregido. Se quito el reverse() en el punto que pedia mostrar el arreglo invertido. Se agregaron prints para que quede mas legible y bello. Se corrigieron algunos para hacerlos como quieres los profesores que los hagamos.

--- 07_activities_3/02_a-g.py
+++ 07_activities_3/02_a-g.py
@@ -1,29 +1,51 @@
-#2#
+arreglo = []
 
-arreglo = [1,2,3,4,5,6,7,8,9,0]
+for i in range(10):
+    numero = int(input("Ingrese un numero: "))
+    arreglo.append(numero)
+
 #a#
-print(arreglo[3])
+print(f"El cuarto componente es {arreglo[3]}")
+
 #b#
-arreglo.reverse()
-print(arreglo)
+arreglo_inv = []
+for i in range(len(arreglo)-1, -1, -1):
+    arreglo_inv.append(arreglo[i])
+
+print(f"El arreglo invertido seria:")
+for i in range(len(arreglo_inv)):
+    print(arreglo_inv[i], end = " ")
+
 #c#
-print(arreglo[0]*arreglo[-1])
+producto = arreglo[0]*arreglo[-1]
+print(f"\nEl producto entre el primer y ultimo componente es {producto}")
+
 #d#
-for i in arreglo:
-    if i%2 == 1:
-        print(i, end=" ")
+print("Se imprimen los componentes de indice impar:")
+for i in range(1, len(arreglo), 2):
+    print(arreglo[i], end= " ")
+
 #e#
-o_suma = 0
-for o in arreglo:
-    if o%2 == 0:
-        o_suma += o
-print("\n",o_suma)
+for i in range(0, len(arreglo), 2):
+    if i == 0:
+        suma = arreglo[i]
+    else:
+        suma += arreglo[i]
+print(f"\nLa suma de los componentes de indice par es de {suma}")
+
 #f#
-i_multiply = 1
-for i in arreglo:
-    if i%2 == 1:
-        i_multiply *= i
-print(i_multiply)
+for i in range(1, len(arreglo), 2):
+    if i == 1:
+        multiply = arreglo[i]
+    else:
+        multiply *= arreglo[i]
+print(f"La multiplicacion de los componentes de indice impar es de {multiply}")
+
 #g#
-arreglo[0], arreglo[-1] = arreglo[-1], arreglo[0]
-print(arreglo)
+aux = arreglo[-1]
+arreglo[-1] = arreglo[0]
+arreglo[0] = aux
+
+print(f"El arreglo con el resultante del primer y ultimo valor intercambiados es de:")
+for i in range(len(arreglo)):
+    print(arreglo[i], end = " ")

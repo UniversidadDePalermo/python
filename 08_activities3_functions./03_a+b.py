86d439a423b4861810c749ff31c98ddae54cb9f6
canabal97 <112731537+canabal97@users.noreply.github.com>
2022-10-28 21:50:19 -0300

    Update 03_a+b.py

--- 08_activities3_functions./03_a+b.py
+++ 08_activities3_functions./03_a+b.py
@@ -1,15 +1,13 @@
-def ingresar_menos15_para2arreglos(arreglo, arreglo2):
+def cantidad_15():
     elementos = int(input("Ingresar la cantidad de elementos que contendran ambos arreglos (menor a 15): "))
     while elementos >= 15:
         elementos = int(input("ERROR: Debe ingresar una cantidad de elementos menores a 15 que contendran ambos arreglos. Intentelo nuevamente: "))
-    for i in range(elementos):
-        numero = int(input("Ingrese un numero para el primer arreglo: "))
-        arreglo.append(numero)
-    for i in range(elementos):
-        numero = int(input("Ingrese un numero para el segundo arreglo: "))
-        arreglo2.append(numero)
     return elementos
-    return numero
+
+def ingresar(arreglo, cantidad):
+    for i in range(cantidad):
+        numero = int(input("Ingrese un numero para el arreglo: "))
+        arreglo.append(numero)
 
 def mostrar(arreglo):
     for i in range(len(arreglo)):
@@ -19,9 +17,13 @@ a = []
 b = []
 c = []
 
-ingresar_menos15_para2arreglos(a, b)
+cantidad = cantidad_15()
+print("Arreglo A:")
+ingresar(a, cantidad)
+print("Arreglo B:")
+ingresar(b, cantidad)
 
 c = a+b
 
-print("El arreglo c terminado es:")
+print("El arreglo C terminado es:")
 mostrar(c)
